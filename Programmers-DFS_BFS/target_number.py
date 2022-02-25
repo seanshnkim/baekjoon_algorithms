3f870aad68cf1f3a3f7d12b173f9beda60300161
sehyunkim <sehyun.seankim@gmail.com>
2022-02-26 00:14:20 +0900

    another solution to target_number.py

--- Programmers-DFS_BFS/target_number.py
+++ Programmers-DFS_BFS/target_number.py
@@ -37,5 +37,23 @@ test_case2 = [4,1,2,1]
 # print(solution(test_case2, 5))
 
 print(solution2(test_case1, 3))
-#print(solution2(test_case2, 4))
+print(solution2(test_case2, 4))
 
+# 다른 사람의 풀이 : best
+def solution_best(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbersPt[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+
+# python의 itertools.product과 map을 이용한 풀이
+# 예를 들어 numbers = [1,2,3,4,5]이라고 하자.
+# 1,2,3,4,5 각 숫자를 더하거나 뺄 수 있는 경우는 2가지(+, -).
+# 각 숫자를 더하거나 빼서 연산하는 경우는 따라서 2**5 = 32가지 (이걸 itertools.product로 Cartesian products를 만들어서 구한다)
+from itertools import product
+def solution_map(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
