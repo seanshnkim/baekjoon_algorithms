2523f33eee73514bb38ac20658163eabc2e22aac
sehyunkim <sehyun.seankim@gmail.com>
2023-01-22 22:07:03 +0900

    bj_6064 x, y fixed

--- Brute Force/bj_6064_카잉달력_ver2.py
+++ Brute Force/bj_6064_카잉달력_ver2.py
@@ -1,23 +1,16 @@
 import sys
 import math
 
+# 이렇게 풀 경우 x 또는 y가 NUM1, NUM2와 같을 때 나머지를 반영하지 못한다.
 def solution(NUM1, NUM2, x, y):
     i = 0
-    if NUM1 < NUM2:
-        while i < NUM2:
-            cnt = NUM1 * i + x
-            if ((NUM1 * i) % NUM2 + x) % NUM2 == y:
-                return cnt
-            i += 1
-    else:
-        while i < NUM1:
-            cnt = NUM2 * i + y
-            if ((NUM2 * i) % (NUM1) + y) % NUM1 == x:
-                return cnt
-            i += 1
-            
+    while i < NUM2:
+        cnt = NUM1 * i + x
+        if cnt % NUM2 == y:
+            return cnt
+        i += 1
     return -1
-    
+        
 
 num_test_input = int(sys.stdin.readline())
 
