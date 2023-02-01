8081b3fd606e1556b62c3f1156c2f331fd526af3
sehyunkim <sehyun.seankim@gmail.com>
2023-02-02 08:05:32 +0900

    bj_1699 solved

--- Dynamic Programming/bj_1699_제곱수의합_ver2.py
+++ Dynamic Programming/bj_1699_제곱수의합_ver2.py
@@ -1,22 +1,19 @@
 import sys
 
-MAX_CNT = 100000
 N = int(sys.stdin.readline())
-dp = [0 for _ in range(N+1)]
-dp[1] = 1
+dp = [i for i in range(N+1)]
 
-for i in range(2, N+1):
-    if i**2 <= N:
-        dp[i**2] = 1
-    if dp[i] == 1:
-        continue
-    
-    sqr_idx = 1
-    if dp[i] == 0:
-        dp[i] = MAX_CNT
-    while sqr_idx**2 < i:
-        dp[i] = min(dp[i], dp[i-sqr_idx**2] + 1)
-        sqr_idx += 1
-    
+for i in range(1, N+1):
+    # if i**2 <= N:
+    #     dp[i**2] = 1
+        
+    # if dp[i] == 1:
+    #     continue
 
+    k = 1
+    while k*k <= i:
+        if dp[i-k*k]+1 < dp[i]:
+            dp[i] = dp[i-k*k]+1
+        k += 1
+    
 print(dp[N])
