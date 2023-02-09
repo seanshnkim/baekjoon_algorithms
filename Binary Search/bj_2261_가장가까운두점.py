9f24a01ac6e7798fe216d5af346e50afcd4bb3d5
sehyunkim <sehyun.seankim@gmail.com>
2023-02-09 16:15:42 +0900

    bj_2261 time out 2

--- Binary Search/bj_2261_가장가까운두점.py
+++ Binary Search/bj_2261_가장가까운두점.py
@@ -3,22 +3,26 @@ input = sys.stdin.readline
 
 N = int(input())
 points = [tuple(map(int, input().split())) for _ in range(N)]
-points = sorted(points, key=lambda x: x[0])
+points.sort(key=lambda x: x[0])
 
 
+def dist_sqrd(pA, pB):
+    return (pA[0] - pB[0])*(pA[0] - pB[0]) + \
+           (pA[1] - pB[1])*(pA[1] - pB[1])
+        
+        
 def solution(points, start, end):
-    if (end - start) <= 2:
-        if (end - start) == 1:
-            return (points[end][0] - points[start][0])**2 + \
-                    (points[end][1] - points[start][1])**2
-        elif (end - start) == 2:
-            # FIXME mid = (end - start) // 2 이런 바보같은 실수!
-            mid = (end + start) // 2
-            dist1 = (points[end][0] - points[mid][0])**2 + \
-                    (points[end][1] - points[mid][1])**2
-            dist2 = (points[mid][0] - points[start][0])**2 + \
-                    (points[mid][1] - points[start][1])**2
-            return min(dist1, dist2)
+    if (end - start) == 1:
+        return dist_sqrd(points[end], points[start])
+    
+    elif (end - start) == 2:
+        # FIXME mid = (end - start) // 2 이런 실수 금지
+        mid = (end + start) // 2
+        d1_sqrd = dist_sqrd(points[end], points[mid])
+        d2_sqrd = dist_sqrd(points[end], points[start])
+        d3_sqrd = dist_sqrd(points[mid], points[start])
+            
+        return min(d1_sqrd, d2_sqrd, d3_sqrd)
                 
     mid = (start + end) // 2
     
@@ -35,35 +39,34 @@ def solution(points, start, end):
 
 def merge_find(dist, start, end):
     mid = (start+end) // 2
-    
     ans = dist
-    to_search = []
-    i = 1
-    # dist: squared distance
-    while mid+i < len(points) and (points[mid+i][0] - points[mid][0])**2 <= dist:
-        to_search.append(points[mid+i])
-        i += 1
-    i = 1
-    while mid-i >= 0 and (points[mid][0] - points[mid-i][0])**2 <= dist:
-        to_search.append(points[mid-i])
-        i += 1
-    
     # append points[mid] to to_search as well
-    to_search.append(points[mid])
-    # sort by y coordinate
-    to_search = sorted(to_search, key=lambda x: x[1])
+    to_search = [points[mid]]
+    # dist: squared distance
     
-    for i in range(len(to_search)):
+    for i in range(mid+1, len(points)):
+        x_diff = points[i][0] - points[mid][0]
+        if x_diff*x_diff < ans:
+            to_search.append(points[i])
+            break
+    for i in range(mid-1, -1, -1):
+        x_diff = points[mid][0] - points[i][0]
+        if x_diff*x_diff < ans:
+            to_search.append(points[i])
+            break
+        
+    # sort by y coordinate
+    to_search.sort(key=lambda x: (x[1], x[0]) )
+    
+    for i in range(len(to_search)-1):
         for j in range(i+1, len(to_search)):
-            if (to_search[j][1] - to_search[i][1])**2 > dist:
+            y_diff = to_search[j][1] - to_search[i][1]
+            if y_diff*y_diff >= dist:
                 break
             else:
-                curr_dist = (to_search[j][0] - to_search[i][0])**2 + \
-                            (to_search[j][1] - to_search[i][1])**2
-                
-                ans = min(curr_dist, ans)
+                curr_dist_sqrd = dist_sqrd(to_search[j], to_search[i])
+                ans = min(curr_dist_sqrd, ans)
     
     return ans
-
 
 print(solution(points, 0, len(points)-1))
