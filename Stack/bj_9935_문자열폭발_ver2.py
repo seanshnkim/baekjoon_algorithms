d75a7b188055abdb2514246d13d0efcd31d3c013
sehyunkim <sehyun.seankim@gmail.com>
2023-02-10 17:06:35 +0900

    bj_9935 solved (ver3.py)

--- Stack/bj_9935_문자열폭발_ver2.py
+++ Stack/bj_9935_문자열폭발_ver2.py
@@ -6,34 +6,37 @@ explode = input().rstrip('\n')
 len_input = len(input_str)
 len_exp = len(explode)
 
-output_str = ''
-stack = ''
+output = []
+stack = []
 
-def is_consecutive(output, stack, c):
-    if not (len(output_str) >=2 and stack):
-        return False
-    else:
-        #REVIEW - 파이썬 리스트 인덱싱 복습해야 할 듯
-        # return output[:-2] == stack[-1] + c
-        return output[-2:] == stack[-1] +c
-    
-
+ans = []
 if len_exp == 1:
-    print(''.join([c for c in input_str if c != explode]))
+    ans = [c for c in input_str if c != explode]
 else:
     for i in range(len_input):
         curr = input_str[i]
-        output_str += curr
+        output.append(curr)
         
+        # FIXME: 반례: explode 문자열이 'abab'인 경우
+        # if curr == explode[0]:
         if curr == explode[0]:
-            stack += curr
-        elif is_consecutive(output_str, stack, curr):
-            stack += curr
-            if curr == explode[-1]:
-                stack = stack[:-len_exp]
-                output_str = output_str[:-len_exp]
+            stack.append((curr, 0) )
+        elif is_consecutive(output, stack, curr):
+            stack.append((curr, stack[-1][1]+1) )
+            #FIXME - 반례: input_str이 ababab, explode 문자열이 'abab'인 경우
+            #REVIEW - 폭발 문자열은 같은 문자를 두 개 이상 포함하지 않는다.
+            # -> 그럼 모두 서로 다른 문자로 이루어져 있다는 뜻이 아니었나?
+            # if curr == explode[-1]:
+            if curr == explode[-1] and stack[-1][1] == len_exp-1:
+                # stack = stack[:-len_exp]
+                # output_str = output_str[:-len_exp]
+                for _ in range(len_exp):
+                    stack.pop()
+                    output.pop()
+    ans = output
     
-    if not output_str:
-        print("FRULA")
-    else:
-        print(''.join(output_str))
+    
+if not ans:
+    print("FRULA")
+else:
+    print(''.join(ans))
---
+++ Stack/bj_9935_문자열폭발_ver3.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+input_str = input().rstrip('\n')
+explode = input().rstrip('\n')
+len_input = len(input_str)
+len_exp = len(explode)
+
+stack = []
+erased = [False]*len_input
+
+ans = []
+if len_exp == 1:
+    ans = [c for c in input_str if c != explode]
+else:
+    for i in range(len_input):
+        curr = input_str[i]
+        
+        if curr == explode[0]:
+            stack.append((i, 0))
+        else:
+            if stack:
+                first = stack[-1]
+                if input_str[i] == explode[first[1]+1]:
+                    stack.append((i, first[1]+1) )
+                    
+                    if first[1]+1 == len_exp-1:
+                        for _ in range(len_exp):
+                            f = stack.pop()
+                            erased[f[0]] = True
+                # else:
+                #     while stack:
+                #         stack.pop()
+                else:
+                    stack.clear()
+                        
+    output = [input_str[i] for i in range(len_input) if not erased[i]]
+    ans = output
+    
+if not ans:
+    print("FRULA")
+else:
+    print(''.join(ans))
