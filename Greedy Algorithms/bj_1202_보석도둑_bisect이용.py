d12991b59981779d3aa1b7c0883cd7e2b097cb85
sehyunkim <sehyun.seankim@gmail.com>
2023-02-21 11:23:04 +0900

    bj_1202 time out(using bisect)

--- Greedy Algorithms/bj_1202_보석도둑_bisect이용.py
+++ Greedy Algorithms/bj_1202_보석도둑_bisect이용.py
@@ -17,17 +17,19 @@ bags = [int(input()) for _ in range(N_bags)]
 bags.sort()
 
 answer = 0
+curr_jewels = []
 for bag in bags:
     if bag < jewels[0].weight:
         continue
-    upper_limit_j = bisect.bisect_right(jewels, (bag, 0))
     
-    max_val = 0
-    for i in range(upper_limit_j):
-        curr_jewel = heapq.heappop(jewels)
-        max_val = max(max_val, curr_jewel.price)
-    
-    answer += max_val
+    while jewels:
+        upper_limit_j = bisect.bisect_right(jewels, (bag, 0))
+        max_val = 0
+        for i in range(upper_limit_j):
+            curr_jewel = heapq.heappop(jewels)
+            max_val = max(max_val, curr_jewel.price)
+        
+        answer += max_val
 
 print(answer)
 
