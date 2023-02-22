f0645d41b6fc85666af13447bcdf32dfdb7a9c4d
sehyunkim <sehyun.seankim@gmail.com>
2023-02-22 14:38:48 +0900

    bj_14889 solved, no time out(PyPy3)

--- Bit Mask/bj_14889_스타트와링크_ver2.py
+++ Bit Mask/bj_14889_스타트와링크_ver2.py
@@ -3,9 +3,15 @@ input = sys.stdin.readline
 
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
+board_flat = []
+for i in range(N):
+    for j in range(N):
+        board_flat.append(board[i][j])
+
 
 answer = float('inf')
 
+cnt_for_case = 0
 for i in range(1<<N):
     cnt_teamA = 0
     teamA_idx = []
@@ -21,17 +27,20 @@ for i in range(1<<N):
             teamB_idx.append(j)
     
     if cnt_teamA == N//2:
+        cnt_for_case += 1
         for k in range(N//2):
             for l in range(k+1, N//2):
                 m1_idx = teamA_idx[k]
                 m2_idx = teamA_idx[l]
-                teamA_sum += board[m1_idx][m2_idx] + board[m2_idx][m1_idx]
+                # teamA_sum += board[m1_idx][m2_idx] + board[m2_idx][m1_idx]
+                teamA_sum += board_flat[m1_idx*N+m2_idx] + board_flat[m2_idx*N+m1_idx]
         
         for k in range(N//2):
             for l in range(k+1, N//2):
                 m1_idx = teamB_idx[k]
                 m2_idx = teamB_idx[l]
-                teamB_sum += board[m1_idx][m2_idx] + board[m2_idx][m1_idx]
+                # teamB_sum += board[m1_idx][m2_idx] + board[m2_idx][m1_idx]
+                teamB_sum += board_flat[m1_idx*N+m2_idx] + board_flat[m2_idx*N+m1_idx]
         
         answer = min(answer, abs(teamA_sum - teamB_sum))
 
