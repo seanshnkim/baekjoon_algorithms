3175a0a4dab3e11a1c7eda0afe611ab3a8795e3f
sehyunkim <sehyun.seankim@gmail.com>
2023-03-06 21:00:24 +0900

    bj_1405 SOLVED (each path)

--- Simulation/bj_1405_미친로봇.py
+++ Simulation/bj_1405_미친로봇.py
@@ -2,31 +2,37 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-N, *probs = map(int, input().split())
+N, *probability = map(int, input().split())
 # 동서남북 순서로
-probs = [prob*0.01 for prob in probs]
+probability = [prob*0.01 for prob in probability]
 dx = [0,  0, 1, -1]
 dy = [1, -1, 0,  0]
-visited = [ [False]*(2*N+1) for _ in range(2*N+1) ]
 
 cnt = 0
 prob = 1
 start = (N, N, cnt, prob)
-visited[N][N] = True
-q = deque([start])
 answer = 0
 
-while q:
-    x, y, cnt, prob = q.popleft()
+
+def move(x, y, visited, prob):
+    # 시작점을 포함한다면, 집합 visited의 원소 개수는 이동한 횟수+1
+    if len(visited) == N+1:
+        return prob
     
-    if cnt == N:
-        answer += prob
-        continue
+    total_prob = 0.0
     
     for i in range(4):
         mx, my = x+dx[i], y+dy[i]
-        if not visited[mx][my]:
-            q.append( (mx, my, cnt+1, prob*probs[i]) )
-            visited[mx][my] = True
+        if (mx,my) not in visited and probability[i] > 0:
+            visited.add( (mx, my) )
+            total_prob += move(mx, my, visited, prob*probability[i])
+            # 다시 해제를 해주어야 각자만의 path를 구별해서 가질 수 있다.
+            visited.remove( (mx, my) )
+    return total_prob
 
-print(answer)
+# visited = set( (N,N) )
+# print( move(N, N, visited, 1) )
+visited = set([(N,N) for _ in range(1)])
+# 또는 이것도 가능
+visited = set([(N,N)])
+print(move(N, N, visited, 1))
