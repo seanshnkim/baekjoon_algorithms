dd5bfdd7f736c1fc30b3de5d4de2c63d51b22271
sehyunkim <sehyun.seankim@gmail.com>
2023-03-07 11:41:16 +0900

    bj_17142 SOLVED

--- Simulation/bj_17142_연구소3.py
+++ Simulation/bj_17142_연구소3.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 from itertools import combinations
+from collections import deque
 
 N, n_virus = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N) ]
@@ -19,71 +20,51 @@ def in_board(x, y):
     return 0 <= x < N and 0 <= y < N
 
 
-def proliferate(board, activated_virus):
-    cur_activated = []
-    cnt_no_empty = 0
-    for virus in activated_virus:
-        x, y = virus
+def bfs(start, count_empty):
+    cnt_empty = count_empty
+    q = deque()
+    visited = [[-1]*N for _ in range(N)]
+    for x,y in start:
+        q.append((x,y))
+        visited[x][y] = 0
+    
+    while q:
+        x, y = q.popleft()
+        
+        if cnt_empty == 0:
+            break
         
         for i in range(4):
             mx, my = x+dx[i], y+dy[i]
-            if in_board(mx, my):
+            if in_board(mx, my) and visited[mx][my] == -1:
                 if board[mx][my] == 0 or board[mx][my] == 2:
                     if board[mx][my] == 0:
-                        cnt_no_empty += 1
-                    board[mx][my] = 3
-                    cur_activated.append((mx, my))
+                        cnt_empty -= 1
+                    q.append((mx, my))
+                    visited[mx][my] = visited[x][y] + 1
     
-    return cur_activated, cnt_no_empty
+    cnt_move = 0
+    for r in range(N):
+        for c in range(N):
+            if board[r][c] == 0 and visited[r][c] == -1:
+                return -1
+            elif visited[r][c] > -1 and cnt_move < visited[r][c]:
+                cnt_move = visited[r][c]
+    return cnt_move
 
 
-# def count_empty(board):
-#     cnt = 0
-#     for r in range(N):
-#         for c in range(N):
-#             if board[r][c] == 0:
-#                 cnt += 1
-#     return cnt
-    
+cnt_empty = 0
+for r in range(N):
+    for c in range(N):
+        if board[r][c] == 0:
+            cnt_empty += 1
 
 answers = []
 for comb in combinations(virus_loc, n_virus):
-    # 활성이면 2에서 1을 더해서 3으로 표시하기로 하자(따라서, 여전히 비활성이면 2이다).
-    tmp_board = [row.copy() for row in board]
-    # 그리고 고른 위치 M개(comb)에 대해 활성화 상태로 만들어주어야 함
-    for x,y in comb:
-        tmp_board[x][y] = 3
-    
-    # cnt_empty = sum(sum(1 if item == 0 else 0 for item in board[r]) \
-    #                     for r in range(N))
-    cnt_empty = 0
-    cnt_time = 0
-    for r in range(N):
-        for c in range(N):
-            if board[r][c] == 0:
-                cnt_empty += 1
-    
-    act_virus = list(comb)
-    while cnt_empty > 0:
-        new_act, cnt_no_empty = proliferate(tmp_board, act_virus)
-        cnt_empty -= cnt_no_empty
-        # 즉 이전 cnt와 현재 cnt가 같고, 빈칸이 줄어들지 않는다는 건 더 이상 바이러스를 퍼뜨릴 수 없다는 뜻
-        # 더 이상 증식할 수 없다면(activated_cell 개수가 증가X) 멈추는 걸로 판정하자.
-        # if cnt_empty == cur_empty:
-        if new_act:
-            act_virus.extend(new_act)
-            cnt_time += 1
-        else:
-            cnt_time = -1
-            break
-    
+    cnt = bfs(list(comb), cnt_empty)
     # FIXME - 그리고 수많은 조합 경우의 수 중에 도달할 수 없는 경우도 있고, 도달할 수 있는 경우도 있을 것
-    answers.append(cnt_time)
+    answers.append(cnt)
 
-# cur_ans = answers[0]
-# for ans in answers:
-#     if (cur_ans == -1 and ans != -1) or ans < cur_ans:
-#         cur_ans = ans
 valid_answers = [ans for ans in answers if ans != - 1]
 if valid_answers:
     print(min(valid_answers))
