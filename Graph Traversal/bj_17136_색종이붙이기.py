d2fb2df99515374ba406611cc6e9320c8b1f31c5
sehyunkim <sehyun.seankim@gmail.com>
2023-03-10 23:50:16 +0900

    bj_17136 line 18, 70

--- Graph Traversal/bj_17136_색종이붙이기.py
+++ Graph Traversal/bj_17136_색종이붙이기.py
@@ -10,22 +10,12 @@ visited = [row.copy() for row in board]
 cnt_left = [0, 5, 5, 5, 5, 5]
 
 
-# 최대 10*10 크기의 보드이므로 시간복잡도는 그리 크지 않을 것이다.
-def is_all_covered(visited):
-    for r in range(BSIZE):
-        for c in range(BSIZE):
-            # 즉, 아직 방문하지 않은 (그러나 채워야했던) 칸이 있다면
-            if visited[r][c] == 1:
-                return False
-    return True
-
-
 # (x, y)를 기준으로, 오른쪽과 아래를 탐색해나가면서 색종이로 덮을 수 있는
 # (즉, 각 칸에 담긴 숫자가 모두 1인) 최대 정사각형의 길이를 구한다.
 def find_largest_square(x, y):
     max_width = 1
-    
-    while x+max_width < BSIZE and y+max_width < BSIZE:
+    # max_width가 5가 넘으면 안된다.
+    while x+max_width < BSIZE and y+max_width < BSIZE and max_width <= 5:
         for r in range(max_width):
             for c in range(max_width):
                 if board[x+r][y+c] != 1:
@@ -57,6 +47,7 @@ def next_fill(visited):
 
 
 def solution(start_x, start_y, cnt):
+    # FIXME - 이것도, 고려 안해줬다. -> 모든 반례에 대해 하나씩 테스트해볼 것.
     max_width = find_largest_square(start_x, start_y)
     width = max_width
     # visited[r][c] == 1이기 때문에 max_width 리턴값은 항상 1 이상
@@ -65,33 +56,40 @@ def solution(start_x, start_y, cnt):
     while width >= 1:
         if cnt_left[width] == 0:
             width -= 1
-        else:
-            cnt_left[width] -= 1
-            cover(start_x, start_y, width)
-            next_pos = next_fill(visited)
-            
-            if next_pos != (-1, -1):
-                # 항상 채워야 할(아직 1인) 칸만 solution 함수에 입력해야 함
-                res = solution(next_pos[0], next_pos[1], cnt+1)
-                # 현재 width는 유효하지 않다는 뜻
-                if res == -1:
-                    cnt_left[width] += 1
-                    uncover(start_x, start_y, width)
-                    break
-                else:
-                    # 현재까지의 cnt : cnt, 앞으로의 cnt : res
-                    if cur_cnt == -1 or cur_cnt > cnt + res:
-                        cur_cnt = cnt + res
-                        # 재귀함수니까 다시 visited 해제해주어야 함
-                    cnt_left[width] += 1
-                    uncover(start_x, start_y, width)
-                    width -= 1
-            # 다 채웠다면. 반드시 width = max_width일 때 발생
-            else:
-                cnt_left[width] += 1
-                uncover(r, c, width)
-                return cnt+1
+            # if else문 너무 중첩되니까 헷갈려서 continue로 처리했다.
+            continue
+        
+        cnt_left[width] -= 1
+        cover(start_x, start_y, width)
+        next_pos = next_fill(visited)
+        
+        if next_pos == (-1, -1):
+            cnt_left[width] += 1
+            # 세상에. 이런 실수를?
+            # uncover(r, c, width)
+            uncover(start_x, start_y, width)
+            return cnt + 1
+        
+        # 항상 채워야 할(아직 1인) 칸만 solution 함수에 입력해야 함
+        res = solution(next_pos[0], next_pos[1], cnt+1)
+        # 현재 width 이하로는 유효하지 않다는 뜻
+        if res == -1:
+            cnt_left[width] += 1
+            uncover(start_x, start_y, width)
+            # FIXME - width == max_width 이었다고 해서 바로 -1 불가능하다고 판단하면 안된다.
+            # if width == max_width:
+            #     return -1
+            width -= 1
+            continue
 
+        # 현재까지의 cnt : cnt, 앞으로의 cnt : res(res는 여기서 -1이 아닌 유효한 값인 경우만)
+        if cur_cnt == -1 or cur_cnt > res:
+            cur_cnt = res
+        # 재귀함수니까 다시 visited 해제해주어야 함
+        cnt_left[width] += 1
+        uncover(start_x, start_y, width)
+        width -= 1
+    
     return cur_cnt
 
 
@@ -102,5 +100,10 @@ for r in range(BSIZE):
         if board[r][c] == 1:
             start_x, start_y = r, c
             break
-        
-print(solution(start_x, start_y, 0))
+    if (start_x, start_y) != (-1, -1):
+        break
+
+if (start_x, start_y) != (-1, -1):
+    print(solution(start_x, start_y, 0))
+else:
+    print(0)
