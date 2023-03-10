db0a3930afcc3b90dcd7734059d16014c34443c1
sehyunkim <sehyun.seankim@gmail.com>
2023-03-10 17:22:36 +0900

    bj_1495 SOLVED (used set)

--- Dynamic Programming/bj_1495_기타리스트.py
+++ Dynamic Programming/bj_1495_기타리스트.py
@@ -4,25 +4,26 @@ input = sys.stdin.readline
 N, START, LIMIT = map(int, input().split())
 volumes = list(map(int, input().split()))
 
-dp = [[] for _ in range(N+1)]
-dp[0] = [START]
+dp = [set() for _ in range(N+1)]
+dp[0].add(START)
 
 def solution():
     for i in range(N):
         if not dp[i]:
             return -1
         
-        for prev in dp[i]:
+        cur_list = list(dp[i])
+        for prev in cur_list:
             cur_vol = prev + volumes[i]
             if cur_vol <= LIMIT:
-                dp[i+1].append(cur_vol)
+                dp[i+1].add(cur_vol)
             
             cur_vol = prev - volumes[i]
             if 0 <= cur_vol:
-                dp[i+1].append(cur_vol)
+                dp[i+1].add(cur_vol)
             
     if dp[-1]:
-        return max(dp[-1])
+        return max(list(dp[-1]))
     else:
         return -1
 
