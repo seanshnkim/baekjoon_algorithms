ac60ad184fe532e115990792d509c6fad1df3bb1
sehyunkim <sehyun.seankim@gmail.com>
2023-03-12 19:41:48 +0900

    bj_1520 SOLVEd

--- Dynamic Programming/bj_1520_내리막길_DP.py
+++ Dynamic Programming/bj_1520_내리막길_DP.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+import heapq
 from collections import deque
 
 H, W = map(int, input().split())
@@ -12,43 +13,32 @@ visited = [[False]*W for _ in range(H)]
 dp = [[-1]*W for _ in range(H)]
 dp[0][0] = 1
 
-q = deque([(0,0)])
-while q:
-    x, y = q.popleft()
-    # q에는 같은 위치의 칸이 여러 개 중복해서 들어가 있을 수 있다. 이미 방문 처리한 것이라면 그냥 건너뛰도록 처리한다.
+next_steps = [(-board[0][0], (0, 0))]
+# visited[0][0] = True
+while next_steps:
+    _, (x, y) = heapq.heappop(next_steps)
+    
     if visited[x][y]:
         continue
     visited[x][y] = True
     
     for i in range(4):
         mx, my = x+dx[i], y+dy[i]
-        if (0 <= mx < H and 0 <= my < W) and board[mx][my] < board[x][y] and not visited[mx][my]:
-            if dp[mx][my] == -1:
-                dp[mx][my] = dp[x][y]
+        if (0 <= mx < H and 0 <= my < W) and dp[mx][my] != -1 and board[x][y] < board[mx][my]:
+            if dp[x][y] == -1:
+                dp[x][y] = dp[mx][my]
             else:
-                dp[mx][my] += dp[x][y]
+                dp[x][y] += dp[mx][my]
             
-            q.append((mx, my))
+    for i in range(4):
+        mx, my = x+dx[i], y+dy[i]
+        if (0 <= mx < H and 0 <= my < W) and board[mx][my] < board[x][y] and not visited[mx][my]:
+            # min heap이 기본이기 때문에 - 부호를 붙여준다.
+            heapq.heappush(next_steps, (-board[mx][my], (mx, my))  )
+
 
 answer = dp[H-1][W-1]
 if answer == -1:
     print(0)
 else:
     print(answer)
-    
-'''
-50 45 40 39
-      30 37
-      20
-      19 18
-
-50 45 40 
-      30
-      20
-      19 18
-      
-50
-45
-40 39 20
-      19 18
-'''
