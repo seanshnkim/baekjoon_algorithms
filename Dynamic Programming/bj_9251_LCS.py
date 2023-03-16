1da4e44374a99a12a3f3993d8e3164cd8a89fd4c
sehyunkim <sehyun.seankim@gmail.com>
2023-03-16 18:47:05 +0900

    bj_9251 WRONG

--- Dynamic Programming/bj_9251_LCS.py
+++ Dynamic Programming/bj_9251_LCS.py
@@ -18,56 +18,30 @@ for i in range(lenB-1, -1, -1):
         dp[i][1] = 1 if dp[i][0] != -1 else 0
         continue
     
+    # 코드 수정: 이제 dp[i+1][0]은 strB의 lenB-1~i+1번째까지 문자 중 strA에서 가장 왼쪽에 있던 문자의 idx로 설정
+    # strA[dp[i+1][0]]과 일치할지도 모르므로 +1 해줌
+    # 아냐, +1 해주면 안돼
     if dp[i+1][0] == -1:
-        idx_in_strA = strA.rfind(strB_char)
-        dp[i][0] = idx_in_strA
-        # 아예 현재 strB_char가 strA에 없다면,그냥 [-1,0]으로 냅둬야 한다.
-        if idx_in_strA == -1:
-            continue
-        
-        # FIXME - line 29 추가
-        dp[i][1] = 1
-        # 얘도 마찬가지로 while문 돌려서 dp[i+2][0], dp[i+3][0]... 계속 비교해보면 되지 않나?
-        for j in range(i+2, lenB):
-            if dp[j][0] != -1:
-                if dp[j][0] > idx_in_strA:
-                    dp[i][1] = dp[j][1] + 1
-                    break
-                elif dp[j][0] == idx_in_strA:
-                    dp[i][1] = dp[j][1]
-                    break
-        
+        idx_in_strA = strA.find(strB_char)
     else:
-        # NOTE: rfind(value= start= end=) -> end는 "미만"이다(string slicing 규칙과 동일)
-        idx_in_strA = strA.rfind(strB_char, 0, dp[i+1][0]+1)
-        
-        #  strB_char가 strB[i+1] 문자 기준에서 왼쪽에 없다면, 오른쪽으로 탐색한다.
-        if idx_in_strA == -1:
-            idx_in_strA = strA.rfind(strB_char, dp[i+1][0])
-            dp[i][0] = idx_in_strA
-            
-            # 아예 현재 strB_char가 strA에 없다면,그냥 [-1,0]으로 냅둬야 한다.
-            if idx_in_strA == -1:
-                continue
-            # if idx_in_strA != -1, 최소한 문자 하나는 일치한다는 뜻이므로
-            dp[i][1] = 1
-            
-            for j in range(i+1, lenB):
-                if dp[j][0] != -1:
-                    if dp[j][0] >= idx_in_strA:
-                        if dp[j][0] == idx_in_strA:
-                            dp[i][1] = dp[j][1]
-                        else:
-                            dp[i][1] = dp[j][1] + 1
-                        break
-            
-        else:
-            # WRONG: 바로 오른쪽 옆 걸로 업데이트하는 게 아니라, 가장 큰 값으로 업데이트해야 함
-            dp[i][0] = idx_in_strA
-            dp[i][1] = dp[i+1][1] +1
+        idx_in_strA = strA.rfind(strB_char, 0, dp[i+1][0])
+    
+    if idx_in_strA == -1:
+        # 그렇다면 더 이상 부분수열을 왼쪽으로 계속 붙여나가, 길이를 증가할 수 없다는 뜻이므로 pass
+        dp[i][0] = dp[i+1][0]
+        dp[i][1] = dp[i+1][1]
+        continue
+    # # 같다면?
+    # elif idx_in_strA == dp[i+1][0]:
+    #     # 같아도 그 왼쪽에 idx가 없다면 없다는 거니까 이것도 노상관
+    #     dp[i][0] = dp[i+1][0]
+    #     dp[i][1] = dp[i+1][1]
+    #     continue
+    
+    # idx_in_strA가 있다면
+    else:
+        dp[i][0] = idx_in_strA
+        dp[i][1] = dp[i+1][1] +1
+    
 
-answer = 0
-for idx, length in dp:
-    if length > answer:
-        answer = length
-print(answer)
+print(dp[0][1])
