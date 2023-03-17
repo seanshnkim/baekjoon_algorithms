0dae5e7b4e9720554ffb63c7f86125a29edb4e69
sehyunkim <sehyun.seankim@gmail.com>
2023-03-17 10:38:22 +0900

    bj_1958 LCS3 SOLVED

--- Dynamic Programming/bj_1958_LCS3.py
+++ Dynamic Programming/bj_1958_LCS3.py
@@ -1,37 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-stringA = input().rstrip('\n')
-stringB = input().rstrip('\n')
-stringC = input().rstrip('\n')
+strA = input().rstrip('\n')
+strB = input().rstrip('\n')
+strC = input().rstrip('\n')
+lenA = len(strA)
+lenB = len(strB)
+lenC = len(strC)
 
-# return the longest common subsequence of strA and strB
-def find_LCS(strA, strB):
-    lenA = len(strA)
-    lenB = len(strB)
-    strA = " " + strA
-    strB = " " + strB
-    dp = [[0]*(lenB+1) for _ in range(lenA+1)]
-    subseqAB = [[""]*(lenB+1) for _ in range(lenA+1)]
+strA = " " + strA
+strB = " " + strB
+strC = " " + strC
 
-    for i in range(1, lenA+1):
-        for j in range(1, lenB+1):
-            if strA[i] == strB[j]:
-                dp[i][j] = dp[i-1][j-1] + 1
-                subseqAB[i][j] = subseqAB[i-1][j-1] + strA[i]
+# dp[0~lenA][0~lenB][0~lenC]
+dp = [ [[0]*(lenC+1) for _ in range(lenB+1)] for _ in range(lenA+1)]
+# subseqAB = [ [[""]*(lenC+1) for _ in range(lenB+1)] for _ in range(lenA+1)]
+
+for i in range(1, lenA+1):
+    for j in range(1, lenB+1):
+        for k in range(1, lenC+1):
+            if strA[i] == strB[j] == strC[k]:
+                dp[i][j][k] = dp[i-1][j-1][k-1] + 1
+                # subseqAB[i][j][k] = subseqAB[i-1][j-1][k-1] + strA[i]
             else:
-                if dp[i-1][j] > dp[i][j-1]:
-                    dp[i][j] = dp[i-1][j]
-                    subseqAB[i][j] = subseqAB[i-1][j]
-                else:
-                    dp[i][j] = dp[i][j-1]
-                    subseqAB[i][j] = subseqAB[i][j-1]
-    
-    return subseqAB[-1][-1]
+                dp[i][j][k] = max(dp[i][j][k-1], dp[i][j-1][k], dp[i-1][j][k])
 
-LCS_ABC = find_LCS(stringC, find_LCS(stringA, stringB) )
-print(len(LCS_ABC))
-
+print(dp[-1][-1][-1])
 '''
 반례(출처: https://www.acmicpc.net/board/view/6859)
 A: dababcf
