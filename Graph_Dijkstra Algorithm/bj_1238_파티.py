bf95e84b2a1a201fb90f6005fe2d0284a0b55f10
sehyunkim <sehyun.seankim@gmail.com>
2023-03-29 22:44:36 +0900

    bj_1238 SOLVED

--- Graph_Dijkstra Algorithm/bj_1238_파티.py
+++ Graph_Dijkstra Algorithm/bj_1238_파티.py
@@ -13,30 +13,21 @@ for _ in range(M):
     graph[start].append((weight, end))
 
 
-def dijkstra_longest(start, dest):
-    # 최장 경로를 구하는 것이기 때문에 float('inf')가 아니라 0으로 초기화해준다.
-    dist = [0]*(N+1)
-    # dist[start] = 0 -> 어차피 0으로 모두 초기화했기 때문에 이 코드는 필요 X
+def dijkstra(start, dest):
+    dist = [float('inf')]*(N+1)
+    dist[start] = 0
     q = []
     heapq.heappush(q, (dist[start], start))
     
     while q:
         cur_dist, cur_node = heapq.heappop(q)
-        cur_dist *= -1
-        
-        # 최단 경로를 구할 땐 어차피 한번 목적지에 도달하면
-        # 경로 가중치 합이 늘어나기만 하기 때문에 더 이상 알아서 탐색을 수행하지 않지만,
-        # 여기선 최장 경로를 구하고 있으므로 한번 이상 목적지(dest)에 도달해도
-        # greedy algorithm 원리에 따라 이웃 노드를 계속 탐색할 수 있다. 따라서 이걸 방지해야 함
-        if cur_node == dest:
-            continue
         
         for adj_w, adj_node in graph[cur_node]:
             next_dist = cur_dist + adj_w
-            # 최단 경로가 아니라 최장 경로를 구하는 과정임을 명심
-            if dist[adj_node] < next_dist:
+            
+            if dist[adj_node] > next_dist:
                 dist[adj_node] = next_dist
-                heapq.heappush(q, ( (-1)*next_dist, adj_node ) )
+                heapq.heappush(q, ( next_dist, adj_node ) )
     
     return dist[dest]
 
@@ -46,11 +37,11 @@ def dijkstra_longest(start, dest):
 k2X = []
 X2k = []
 for start in range(1, N+1):
-    k2X.append(dijkstra_longest(start, X))
-    X2k.appennd(dijkstra_longest(X, start))
+    k2X.append(dijkstra(start, X))
+    X2k.append(dijkstra(X, start))
 
 answer = 0
 for dA, dB in zip(k2X, X2k):
-    if answer > dA + dB:
+    if answer < dA + dB:
         answer = dA + dB
 print(answer)
