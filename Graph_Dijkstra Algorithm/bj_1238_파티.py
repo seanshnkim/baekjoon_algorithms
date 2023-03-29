95d11edb3d12789543b57fb583a8125f59baccc1
sehyunkim <sehyun.seankim@gmail.com>
2023-03-29 22:50:04 +0900

    bj_1238 refactored

--- Graph_Dijkstra Algorithm/bj_1238_파티.py
+++ Graph_Dijkstra Algorithm/bj_1238_파티.py
@@ -1,9 +1,6 @@
 import sys
-input = sys.stdin.readline
-
-import sys
-input = sys.stdin.readline
 import heapq
+input = sys.stdin.readline
 
 N, M, X = map(int, input().split())
 graph = [[] for _ in range(N+1)]
@@ -32,16 +29,8 @@ def dijkstra(start, dest):
     return dist[dest]
 
 
-# 각 1~N번 노드에 대해 X번까지의 최장 경로를 구하고, 다시 X번부터 1~N번까지의 최장 경로를 구한다.
-# k번 노드 ~ X번 노드 최장 경로 + X번 노드 ~ k번 노드 최장 경로 합이 가장 큰 경우를 출력한다.
-k2X = []
-X2k = []
+round_trip_dist = []
 for start in range(1, N+1):
-    k2X.append(dijkstra(start, X))
-    X2k.append(dijkstra(X, start))
+    round_trip_dist.append( dijkstra(start, X) + dijkstra(X, start) )
 
-answer = 0
-for dA, dB in zip(k2X, X2k):
-    if answer < dA + dB:
-        answer = dA + dB
-print(answer)
+print(max(round_trip_dist))
