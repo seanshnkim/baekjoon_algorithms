c79a22ac500d65d092e180e11559e57dee86a89f
sehyunkim <sehyun.seankim@gmail.com>
2023-03-30 00:12:27 +0900

    bj_2146 SOLVED (time complexity improved)

--- Graph Traversal/bj_2146_다리만들기.py
+++ Graph Traversal/bj_2146_다리만들기.py
@@ -26,35 +26,31 @@ def bfs_for_land(start_x, start_y, idx_island):
                 q.append((mx, my))
 
 
-def bfs_for_path(start_x, start_y, cur_island):
-    q = deque([(start_x, start_y, 0)])
-    tmp_visited = [[False]*N for _ in range(N)]
-    tmp_visited[start_x][start_y] = True
+def bfs_for_path(cur_island):
+    dist = [[-1]*N for _ in range(N)]
+    q = deque()
     
-    min_path = 2*N
-    adj_island = 0
+    for r in range(N):
+        for c in range(N):
+            if board[r][c] == cur_island:
+                q.append((r, c))
+                dist[r][c] = 0
     
     while q:
-        x, y, d = q.popleft()
-        
-        if board[x][y] > 0 and board[x][y] != cur_island:
-            min_path = d-1
-            adj_island = board[x][y]
-            break
+        x, y = q.popleft()
         
         for i in range(4):
             mx, my = x+dx[i], y+dy[i]
 
-            if 0 <= mx < N and 0 <= my < N and not tmp_visited[mx][my]:
-                tmp_visited[mx][my] = True
-                next_dist = 0
-                
-                if board[mx][my] == cur_island:
-                    q.append((mx, my, 0))
-                else:
-                    q.append((mx, my, d+1))
-    
-    return min_path, adj_island
+            if not (0 <= mx < N and 0 <= my < N):
+                continue
+            
+            if board[mx][my] > 0 and board[mx][my] != cur_island:
+                return dist[x][y]
+            
+            if board[mx][my] == 0 and dist[mx][my] == -1:
+                dist[mx][my] = dist[x][y] + 1
+                q.append((mx, my))
 
 
 
@@ -66,16 +62,10 @@ for r in range(N):
             bfs_for_land(r, c, idx_island)
             idx_island += 1
             
-
+# 각 섬 사이 최단 경로를 구한다.
 answer = float('inf')
-visited_islands = set()
-for cur_island in range(1, idx_island+1):
-    for r in range(N):
-        for c in range(N):
-            if board[r][c] == cur_island:
-                cur_min, adj_island = bfs_for_path(r, c, cur_island)
-                visited_islands.add((cur_island, adj_island))
-                
-                answer = min(answer, cur_min)
+for cur_island in range(1, idx_island):
+    cur_min = bfs_for_path(cur_island)
+    answer = min(answer, cur_min)
 
 print(answer)
