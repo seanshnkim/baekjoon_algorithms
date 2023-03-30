d611ddab1354deacf202545f7c541b23b3fcb5ea
sehyunkim <sehyun.seankim@gmail.com>
2023-03-30 16:34:55 +0900

    bj_17074 wrong wrong wrong

--- Dynamic Programming/bj_17074_정렬.py
+++ Dynamic Programming/bj_17074_정렬.py
@@ -9,43 +9,37 @@ is_sorted = True
 i = 0
 while i < N-1:
     if numbers[i] > numbers[i+1]:
-        if cnt == 1:
+        is_sorted = False
+        
+        if cnt >= 1:
             cnt = 0
+            break
+        if i == N-2:
+            if numbers[i-1] <= numbers[i+1]:
+                cnt = 2
+            else:
+                cnt = 1
             break
         
         if i == 0:
-            left_max = -float('inf')
-        else:
-            left_max = numbers[i-1]
-
-        if numbers[i+1] >= left_max:
-            if i == N-2:
-                cnt = 2
+            cnt += 1
+        elif numbers[i+1] >= numbers[i-1]:
+            cnt += 1
+        
+        i += 1
+        if numbers[i-1] <= numbers[i+1]:
+            cnt += 1
+            
+        elif numbers[i-1] > numbers[i+1]:
+            if numbers[i] > numbers[i+1]:
+                break
             else:
                 cnt += 1
-                i += 1
-                if numbers[i] > numbers[i+1]:
-                    cnt = 0
-                    break
-                if numbers[i-1] > numbers[i+1]:
-                    cnt = 0
-                    break
-                if numbers[i-1] <= numbers[i+1]:
-                    cnt += 1
-        else:
-            if i == N-2:
-                cnt = 1
-            else:
-                i += 1
-                if numbers[i] > numbers[i+1]:
-                    break
-                if numbers[i-1] > numbers[i+1]:
-                    break
-                else:
-                    cnt += 1
+                
     i += 1
 
-if N == 2:
-    print(2)
+
+if is_sorted:
+    print(N)
 else:
     print(cnt)
