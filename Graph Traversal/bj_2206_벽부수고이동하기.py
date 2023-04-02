9ca67709bf5a65d59c178eb3a7557387675b0544
sehyunkim <sehyun.seankim@gmail.com>
2023-04-02 16:14:33 +0900

    bj_2206 MEMORY OVERFLOW

--- Graph Traversal/bj_2206_벽부수고이동하기.py
+++ Graph Traversal/bj_2206_벽부수고이동하기.py
@@ -10,44 +10,36 @@ board = [[int(i) for i in input().rstrip('\n')] for _ in range(H)]
 dx = [-1, 1, 0, 0]
 dy = [0 , 0, 1, -1]
 
-def bfs_with_wall(start_x, start_y, cnt_break):
-    # visited[x][y] == -1: 아직 방문 X
-    # visited[x][y] == 0: 벽을 부수지 않고 방문한 칸
-    # visited[x][y] == 1: 벽을 한번 부수고(부수어야) 방문한 칸
-    # visited[x][y] == 2: 벽을 2번 이상 부수어야 방문할 수 있는 칸, 더 이상 탐색하지 않음.
+def bfs_with_wall(start_x, start_y):
     visited = [[-1]*W for _ in range(H)]
     answer = -1
-    # cnt_break = 0
-    q = deque([(0, start_x, start_y)])
+    # q의 원소 튜플: cur_dist, x좌표, y좌표
+    q = deque([(0, 0, start_x, start_y)])
     visited[start_x][start_y] = 0
     
     while q:
-        d, x, y = q.popleft()
+        d, cur_break, x, y = q.popleft()
         
         if x == H-1 and y == W-1:
             if answer == -1 or answer > d:
                 answer = d
             break
         
-        is_move_possible = False
+        if cur_break == 2:
+            continue
+        
         for i in range(4):
             mx, my = x+dx[i], y+dy[i]
             
-            if 0 <= mx < H and 0 <= my < W and visited[mx][my] == -1:
+            # 이제는 visited[mx][my] == -1일 때만 탐색 X
+            # 벽을 한번 뚫고 탐색한 경우(visited -> 0), 그 다음에 벽을 뚫지 않고 탐색한 경우 (visited -> 1)
+            # 따라서, visited 값이 1 이상이면 방문 완료한 ?
+            if 0 <= mx < H and 0 <= my < W and visited[mx][my] < 1:
                 if board[mx][my] == 0:
-                    is_move_possible = True
-                    q.append((d+1, mx, my))
-                    visited[mx][my] = visited[x][y]
-                    
-        if not is_move_possible:
-            if visited[x][y] >= 1:
-                continue
-            
-            for i in range(4):
-                mx, my = x+dx[i], y+dy[i]
-                if 0 <= mx < H and 0 <= my < W and visited[mx][my] == -1:
-                    q.append((d+1, mx, my))
-                    visited[mx][my] = visited[x][y] + 1
+                    q.append((d+1, cur_break, mx, my))
+                else:
+                    q.append((d+1, cur_break+1, mx, my))
+
     
     if answer == -1:
         return answer
@@ -64,4 +56,4 @@ def bfs_with_wall(start_x, start_y, cnt_break):
 000010
 '''
 
-print(bfs_with_wall(start_x=0, start_y=0, cnt_break=0))
+print(bfs_with_wall(start_x=0, start_y=0))
