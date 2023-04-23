c730ea6c44ba217b299bbd0f88b813affe671695
sehyunkim <sehyun.seankim@gmail.com>
2023-04-23 15:59:02 +0900

    bj_2887 SOLVED

--- Graph_Kruskal/bj_2887_행성터널.py
+++ Graph_Kruskal/bj_2887_행성터널.py
@@ -1,50 +1,25 @@
 import sys
 input = sys.stdin.readline
-import heapq
-
-def cal_dist(xA, yA, zA, xB, yB, zB):
-    return min(abs(xA-xB), abs(yA-yB), abs(zA-zB))
 
 N = int(input())
-planets = [list(map(int, input().split())) for _ in range(N)]
+planets = [list(map(int, input().split())) + [i] for i in range(N)]
+edges = []
 
-X_edges = []
-Y_edges = []
-Z_edges = []
-for i in range(N):
-    for j in range(i+1, N):
-        x_diff = abs(planets[i][0] - planets[j][0])
-        y_diff = abs(planets[i][1] - planets[j][1])
-        z_diff = abs(planets[i][2] - planets[j][2])
-        
-        if len(X_edges) == N:
-            heapq.heappushpop(X_edges, ((-1)*x_diff, i, j))
-        else:
-            heapq.heappush(X_edges, ((-1)*x_diff, i, j))
-            
-        if len(Y_edges) == N:
-            heapq.heappushpop(Y_edges, ((-1)*y_diff, i, j))
-        else:
-            heapq.heappush(Y_edges, ((-1)*y_diff, i, j))
-            
-        if len(Z_edges) == N:
-            heapq.heappushpop(Z_edges, ((-1)*z_diff, i, j))
-        else:
-            heapq.heappush(Z_edges, ((-1)*z_diff, i, j))
-
+for c in range(3):
+    # x,y,z 좌표에 대해 정렬
+    planets.sort(key=lambda x: x[c])
+    for i in range(1, N):
+        # 각 x좌표 간 거리 차이를 구하여 edges에 저장, 어쨌든 여기서 두 점 사이 거리는 
+        # x좌표 간 거리 차이 또는 y좌표 간 거리 차이 또는 z좌표 간 거리 차이 이 셋 중 하나로 정의되었으므로.
+        diff = abs(planets[i][c]-planets[i-1][c])
+        idx_A = planets[i-1][3]
+        idx_B = planets[i][3]
+        edges.append((diff, idx_A, idx_B) )
 
 parent = [*range(N)]
 rank = [0]*N
-edges = []
+edges.sort()
 
-for _, a, b in X_edges:
-    edges.append((a, b, cal_dist(*planets[a], *planets[b])) )
-for _, a, b in Y_edges:
-    edges.append((a, b, cal_dist(*planets[a], *planets[b])) )
-for _, a, b in Z_edges:
-    edges.append((a, b, cal_dist(*planets[a], *planets[b])) )
-
-edges.sort(key=lambda x: x[2])
 
 def find(x):
     if parent[x] == x:
@@ -73,7 +48,7 @@ def rank_merge(a, b):
     return True
 
 answer = 0
-for a, b, w in edges:
+for w, a, b in edges:
     if rank_merge(a, b):
         answer += w
         
