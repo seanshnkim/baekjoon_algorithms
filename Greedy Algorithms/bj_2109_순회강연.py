18ab57e3c78f2cd4fe7d1a1ffa06085594314a97
sehyunkim <sehyun.seankim@gmail.com>
2023-05-16 23:48:40 +0900

    bj_2109 counter example

--- Greedy Algorithms/bj_2109_순회강연.py
+++ Greedy Algorithms/bj_2109_순회강연.py
@@ -7,21 +7,35 @@ N = int(input())
 infos = []
 for _ in range(N):
     p, d = map(int, input().split())
-    heapq.heappush(infos, (-p, d))
+    heapq.heappush(infos, (d, -p))
 
 ans = 0
 time = 0
 
 while infos:
-    cur_p, cur_d = infos[0]
+    cur_d, cur_p = heapq.heappop(infos)
+    cur_p = (-1)*cur_p
     
     if cur_d - time > 0:
-        ans += (-1)*cur_p
-        heapq.heappop(infos)
+        cur_max = cur_p
+        i = 0
+        while infos and infos[0][0] == cur_d:
+            next_p = infos[0][1]
+            if next_p > cur_max:
+                cur_max = next_p
+            # i += 1
+            heapq.heappop(infos)
+        
+        ans += cur_max
         time += 1
+    
     else:
-        heapq.heappop(infos)
+        break
     
     
-
 print(ans)
+
+'''
+반례:
+10 1 10 1 300 2 300 2
+'''
